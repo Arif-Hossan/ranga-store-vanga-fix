05bdae4e6de368c5c1f08dd153b43b490998ba38
Arif Hossan <arif.hossan.eu@gmail.com>
2023-03-12 21:13:43 +0600

    fixing image issues

--- js/app.js
+++ js/app.js
@@ -13,14 +13,16 @@ loadProducts('https://fakestoreapi.com/products');
 
 // show all product in UI
 const showProducts = (products) => {
-   
+
    setInnerText('total_products', products.length);
 
    document.getElementById("all-products").innerHTML = "";
 
    const allProducts = products.slice(0, 10).map((pd) => pd);
+
    for (const product of allProducts) {
-      const image = product.images;
+      const image = product.image;
+       
       const div = document.createElement('div');
       div.classList.add('product');
       div.innerHTML = `<div class="single-product">
@@ -33,7 +35,7 @@ const showProducts = (products) => {
 
       <button onclick="showProductDetails(${product.id})" id="details-btn"    data-bs-toggle="modal"
       data-bs-target="#exampleModal" class="btn btn-outline-secondary mb-2 rounded-1 mt-1">Details</button>
-      
+
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success border-0 w-100 rounded-0 bg-main py-2">Add to cart</button>
       `;
       document.getElementById('all-products').appendChild(div);
